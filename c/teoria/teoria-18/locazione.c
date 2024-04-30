3d4dd5b57f273f63fa3dc52fa61853774f12335b
FrancescoEsk <francifrison.ff@gmail.com>
2024-04-30 10:01:31 +0200

    s

--- c/teoria/teoria-18/locazione.c
+++ c/teoria/teoria-18/locazione.c
@@ -2,8 +2,13 @@
 /*
 ALLOCAZIONE VARIABILI AUTOMATICA o STATICA
 --> STACK
+
 --> L'allocazione avviene in modo automatico quando si entra in un blocco di codice
     e viene riallocata automaticamente quando si esce dal blocco
+
+--> richiedono di conoscere la dimensione delle variabili in tempo di compilazione
+
+
 */
 
 int main(void){
