eaa4b06f5de66fe7ab39ab83abc715985dbc7fd7
FrancescoEsk <francifrison.ff@gmail.com>
2024-04-30 09:59:02 +0200

    s

---
+++ c/teoria/teoria-18/locazione.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ALLOCAZIONE VARIABILI AUTOMATICA o STATICA
+--> STACK
+--> L'allocazione avviene in modo automatico quando si entra in un blocco di codice
+    e viene riallocata automaticamente quando si esce dal blocco
+*/
+
+int main(void){
+
+    return 0;
+}
